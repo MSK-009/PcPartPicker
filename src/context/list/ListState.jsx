21232e809f175c54c6d535553df84bbe85682e07
MSK-009 <luxumen03@gmail.com>
2024-10-26 16:42:16 +0500

    drag

--- src/context/list/ListState.jsx
+++ src/context/list/ListState.jsx
@@ -2,12 +2,27 @@ import { useEffect, useState } from "react"
 import ListContext from "./ListContext";
 
 const ListState = (props) => {
-  const [ selectedItem, setSelectedItem ] = useState({})
+  const [selectedItem, setSelectedItem] = useState({})
 
   const onDragStart = (e, item, listName) => {
     e.dataTransfer.setData('item', JSON.stringify(item));
     e.dataTransfer.setData('listName', listName);
     e.currentTarget.classList.add("dragging");
+
+
+    const dragImage = e.currentTarget.cloneNode(true);
+    dragImage.style.position = "absolute";
+    dragImage.style.opacity = "1"
+    dragImage.style.pointerEvents = "none";
+    dragImage.style.top = `${e.clientY - 20}px`; // Offset for visibility near the cursor
+    dragImage.style.left = `${e.clientX - 20}px`
+
+    document.body.appendChild(dragImage);
+    e.dataTransfer.setDragImage(dragImage, 150, 200)
+
+    setTimeout(() => document.body.removeChild(dragImage), 0);
+
+
   };
 
   const onDragOver = (e) => {
@@ -31,7 +46,7 @@ const ListState = (props) => {
 
   const removeItem = (key) => {
     const { [key]: _, ...updatedSelected } = selectedItem;
-  setSelectedItem(updatedSelected);
+    setSelectedItem(updatedSelected);
   }
 
 
